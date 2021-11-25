66179f3646b7e3a1b0deb52ac61e0b76f2380a64
NPraglin <87403173+NPraglin@users.noreply.github.com>
2021-11-24 19:59:06 -0800

    Create Sumofdigits.js

---
+++ Sumofdigits.js
@@ -0,0 +1,15 @@
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+// ANSWER
+
+function digital_root(n) {
+  if (n < 10) return n;
+  // transforming number to string, splitting to array of strings, reducing using accumulator and currentvalue, +val converts to number
+  return digital_root((n + '').split('').reduce((acc, val)=> acc + +val,0));
+}
